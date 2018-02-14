6ac1adc89b0633d1b331c8268b674174dd1fe6c5
Edgar Acuna <eacunaf@yahoo.com>
2018-02-14 06:35:30 -0400

    Add files via upload

--- rlab3.R
+++ rlab3.R
@@ -4,29 +4,37 @@
 # Edgar Acuna
 #************************************************************
 #Leyendo el archivo de datos corrs directamente desde la internet
-cor1=read.table(file="http:/academic.uprm.edu/eacuna/corrs.dat",header=T)
+cor1=read.table(file="http://academic.uprm.edu/eacuna/corrs.dat",header=T)
+
 co1=cor(cor1$x1,cor1$y1)
 cat("\n","el coeficiente de correlacion entre X1 y Y1 es=",co1,"\n")
+
 co2=cor(cor1$x2,cor1$y2)
 cat("\n","el coeficiente de correlacion entre X2 y Y2 es=",co2,"\n")
+
 co3=cor(cor1$x3,cor1$y3)
-cat("\n","el coeficiente de correlacion entre X3 y Y3 es=",co3,"\n") 
+cat("\n","el coeficiente de correlacion entre X3 y Y3 es=",co3,"\n")
+
 co4=cor(cor1$x4,cor1$y4)
 cat("\n","el coeficiente de correlacion entre X4 y Y4 es=",co4,"\n")
+
 win.graph()
 par(mfrow=c(2,2),oma=c(1,1,1,1))
 plot(cor1$x1,cor1$y1)
 text(10,30,"r=.988")
+
 plot(cor1$x2,cor1$y2)
 text(10,20,"r=-.992")
 plot(cor1$x3,cor1$y3)
 text(12,32,"r=.191")
+
 plot(cor1$x4,cor1$y4)
 text(12,25,"r=.112")
 title("Ejemplos de correlaciones",outer=TRUE)
+
 #Leyendo el archvo de datos corrout
- cor2<-read.table(file="http://math.uprm.edu/~edgar/corrout.dat",header=T)
- attributes(cor2)
+ cor2<-read.table(file="http://academic.uprm.edu/eacuna/corrout.dat",header=T)
+attributes(cor2)
  c1<-cor(cor2$x1,cor2$y1)
 cat("\n ","el coeficiente de correlacion entre X1 y Y1 es=",c1,"\n")
  c2<-cor(cor2$x2,cor2$y2)
